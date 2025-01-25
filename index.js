27bd100229bfcb069affe2e0134dc92201909ca7
Arthur <arthurschiller10@gmail.com>
2025-01-25 00:30:11 -0300

    .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Porta do servidor
@@ -18,7 +20,8 @@ app.post('/calculate-shipping', async (req, res) => {
   }
 
   const apiUrl = 'https://www.melhorenvio.com.br/api/v2/me/shipment/calculate';
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYjQ4MTJjYmUzNjNhZWExMjE2Mjc3M2M5ZmJjYzlkYWE1MmZjNjhjMDg0MmQ3OTk1YzUxNzBiZDI2ZTZhMTE0YjE5ZTE4OGFkMjUyYjUxZGQiLCJpYXQiOjE3Mzc2ODkyNjUuNDQ5ODI3LCJuYmYiOjE3Mzc2ODkyNjUuNDQ5ODI4LCJleHAiOjE3NjkyMjUyNjUuNDM3ODc1LCJzdWIiOiI5OWRjMDRkZS04NGY3LTQ1YWItOTcwYi03ZTM4NTUyNjU3NzkiLCJzY29wZXMiOlsic2hpcHBpbmctY2FsY3VsYXRlIl19.obexJl9kjJ9swGAYZ6YiEdas9rUeljq222Apc2A4VlT-gQ0ofyzP_7PHhIXWyTInanTqN4PMxtOrcQgxTIhK2aeUQ6duims8P3rZxkQNspQ_uewBTID-vFvkKSZ6CGBcmhHFpKGDZkWAKGnFcP3zeYSAoNV7S_4K8c5PnhFuficS9VDVJJKGwhfEYACTNPN6DTqCh3jHTTiyMMze9V4eRm5hLNMPwgX5lSjXEdnQuENhxu74LZDcjg1vYWfu12-WgP0gZ-Jcs9Fnk_S4j7rWC6oRbb9lDZ0Z8iEaGTc7hzJ7rdOEhsYWm8pe94sAw3YZ-vAlfbzDbE6uiej6rfNOsqdY8qJEm3iMUHIAeG9ZbZEpR1zvUjg_GG9ulsIMJjUHa-GbfWXohF3ZgX5eTFgf387HaNjkqCIQ0Qged8GfSpjPxDb9J6iH6Ia7QeLM1duy05pU_8qQONOCYYyRYUVQ-ZEmtqfjFNG7Vy3PP5A4vi-EFFhL3PymUUEQoWfyE4i8pt1qlh8YHCJiBWn1e27HoFv26ZBIiy7eytRB66OAVH4-1Q-f4Mdcp_OHweB-Z3arvdR3O1pOvfqSZ3ffPMcyrEBPTYydHrVTGjrLfiSJSKYiF_7V_urWE6t5Yq-awOsG7DKskUy-ZWviHEU_RGx_9cIYmRQ0J_1bmzs-R4f7hy0'
+  const token = process.env.API_KEY;
+
 
   const requestBody = {
     from: { postal_code: '34006065' }, // CEP da loja
